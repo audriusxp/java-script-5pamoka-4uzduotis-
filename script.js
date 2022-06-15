0b4039e3d1e6864d65f02afadc3dc39c53a88531
audriusxp <palm@inbox.lt>
2022-06-15 14:53:31 +0300

    6 punktas, nesigavo, nes isveda lyginius skaicius ir ju nesudeda

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 window.onload = function(){
     const testinis =[];
-    const nelyginiaiNuo107iki309 = issaugotiVisusNelyginius107Iki309();
+    const nelyginiaiNuo107iki309 = issaugotiVisusNelyginiusXikiY(107,309);
     console.log(nelyginiaiNuo107iki309);
     console.log(testinis);
     const suma = masyvoSuma(nelyginiaiNuo107iki309);
     console.log(suma);
+    const nelyginiaiNuo100iki104 =issaugotiVisusNelyginiusXikiY(100,104);
+    console.log(nelyginiaiNuo100iki104);
+    
     
 }
-function issaugotiVisusNelyginius107Iki309() {
+function issaugotiVisusNelyginiusXikiY(x,y) {
     const testinis = [];
-    let x = 1;
-    let y = 1000;
+    
     for (let i = x; i <= y; i += 2) {
         testinis.push(i);
     }
     return testinis;
 }
-
-
-    
-
 function arNelyginis(x){
     return x % 2 > 0;
 }
