5218a6e9c10c43cc25821590490b3c139445518f
audriusxp <palm@inbox.lt>
2022-06-15 13:32:16 +0300

    isaugoji nelyginiai skaiciai nuo 107 iki 309

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function(){
+    const testinis =[];
+    const nelyginiaiNuo107iki309 = issaugotiVisusNelyginius107Iki309();
+    console.log(nelyginiaiNuo107iki309);
+    console.log(testinis);
     
+}
+function issaugotiVisusNelyginius107Iki309() {
+    const testinis = [];
+    for (let i = 107; i <= 309; i += 2) {
+        testinis.push(i);
+    }
+    return testinis;
 }
